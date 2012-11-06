2f09abd09eab6292871fc54ffcebed915a2f8d95
Alfonso Palacio <alfonso.a.palacio>
2012-11-06 11:04:56 -0500

    server ready

--- chat.js
+++ chat.js
@@ -1,21 +1,51 @@
 var net = require('net');
 
 var chatServer =  net.createServer(),
+	cleanup = [],
 	clientList = [];
 
-chatServer.on('connection', function(client){
-	client.write('Hi \n');
+function broadcast(message, client){
 
-	clientList.push(client);
+		
 
-	client.on('data', function(data){
-	
 		for (var i = 0;  i < clientList.length; i+=1) {
-			//write this data to all clients
-			clientList[i].write(data);
+			if (client!=clientList[i]) {
+				if(clientList[i].writable){
+					clientList[i].write(client.name + 'says '+ message);	
+				}
+				else
+				{
+					cleanup.push(clientList[i]);
+					clientList[i].destroy();
+				}				
+			};
 		};		
+};
+
+
+chatServer.on('connection', function(client){
+	client.name = client.remoteAddress + ':' + client.remotePort;	
+	client.write('Hi'+ client.name +'\n');
+	console.log(client.name + ' joined');
+	clientList.push(client); 
+
+	client.on('data', function(data){	
+		broadcast(data, client);
 	});
 
-}).listen(9000);
+	//Remove dead Nodes out of write loop to avoid trashing loop index
+	client.on('end', function(){
+		console.log(client.name + ' quit');
+		for(i=0; i<cleanup.length; i+=1){
+			clientList.splice(clientList.indexOf(client),1);		
+		}
+	});
+
+	client.on('error', function(e){
+		console.log(e);
+	});
+});
+
+chatServer.listen(9000);
 
 console.log("Chat sever initiated");
